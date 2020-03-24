0f4e595e21aaa8378b7c242438cf9e01d32adbb2
Igor Sales <igorsales.fs@gmail.com>
2020-03-24 12:29:44 -0300

    conectando ao MongoDB com repository e service

--- src/main/java/com/igorsales/demo/resources/UserResource.java
+++ src/main/java/com/igorsales/demo/resources/UserResource.java
@@ -1,27 +1,27 @@
 package com.igorsales.demo.resources;
 
 import com.igorsales.demo.domain.User;
+import com.igorsales.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User jose = new User("1", "Jose Carlos","josecarlos@gmail.com");
-        User igor = new User("2", "Igor Sales","igorsales.fs@gmail.com");
-        User carlos = new User("3", "Carlos Alberto","carlos@gmail.com");
 
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(jose, igor, carlos));
+        List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
