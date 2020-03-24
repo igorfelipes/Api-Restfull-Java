c9aad690e4ce0b1720b0280836592ee587f5102b
Igor Sales <igorsales.fs@gmail.com>
2020-03-24 16:25:09 -0300

    obter usuario pelo id e criacao de exceptions

--- src/main/java/com/igorsales/demo/resources/UserResource.java
+++ src/main/java/com/igorsales/demo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.igorsales.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,10 @@ public class UserResource {
         List<User> list = userService.findAll();
         List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
